6dcd778e2b2243ba790a8c9d7dabe343e13f4531
DonizeteZJunior <91087078+DonizeteZJunior@users.noreply.github.com>
2024-02-14 09:52:23 -0300

    Projeto finalizado. 14-02-2024.

--- 32_DEV_NOTES/js/script.js
+++ 32_DEV_NOTES/js/script.js
@@ -2,6 +2,8 @@
 const notesContainer = document.querySelector("#notes-container");
 const noteInput = document.querySelector("#note-content");
 const addNoteBtn = document.querySelector(".add-note");
+const searchInput = document.querySelector("#search-input");
+const exportBtn = document.querySelector("#export-notes");
 
 //Funções
 function showNotes() {
@@ -55,14 +57,36 @@ function createNote(id, note, fixed){
     pinIcon.classList.add(...["bi","bi-pin"]);
     element.appendChild(pinIcon);
 
+    const deleteIcon = document.createElement("i");
+    deleteIcon.classList.add(...["bi","bi-x-lg"]);
+    element.appendChild(deleteIcon);
+
+    const duplicateIcon = document.createElement("i");
+    duplicateIcon.classList.add(...["bi","bi-file-earmark-plus"]);
+    element.appendChild(duplicateIcon);
+
     if(fixed){
         element.classList.add("fixed");
     }
 
     //Eventos do elemento
+    element.querySelector("textarea").addEventListener("keyup", (e) => {
+        const noteContent = e.target.value;
+
+        updateNote(id, noteContent);
+    });
+
     element.querySelector(".bi-pin").addEventListener("click", () => {
         toggleFixNote(id);
     });
+
+    element.querySelector(".bi-x-lg").addEventListener("click", () => {
+        deleteNote(id, element);
+    });
+
+    element.querySelector(".bi-file-earmark-plus").addEventListener("click", () => {
+        copyNote(id);
+    })
 
     return element;
 }
@@ -78,6 +102,43 @@ function toggleFixNote(id){
     showNotes();
 }
 
+function deleteNote(id, element) {
+    const notes = getNotes().filter((note) => note.id !== id);
+
+    saveNotes(notes);
+
+    notesContainer.removeChild(element);
+}
+
+function copyNote(id){
+    const notes = getNotes();
+    const targetNote = notes.filter((note) => note.id === id)[0];
+
+    const noteObject = {
+        id: generateId(),
+        content: targetNote.content,
+        fixed: false,
+    };
+
+    const noteElement = createNote(
+        noteObject.id,
+        noteObject.content, 
+        noteObject.fixed);
+
+    notesContainer.appendChild(noteElement);
+    notes.push(noteObject);
+    saveNotes(notes);
+}
+
+function updateNote(id, newContent){
+    const notes = getNotes();
+    const targetNote = notes.filter((note) => note.id === id)[0];
+
+    targetNote.content = newContent;
+
+    saveNotes(notes);
+}
+
 // Local Storage
 function getNotes(){
     const notes = JSON.parse(localStorage.getItem("notes")|| "[]");
@@ -91,8 +152,55 @@ function saveNotes(notes) {
     localStorage.setItem("notes",JSON.stringify(notes));
 }
 
+function searchNotes(search){
+    const searchResults = getNotes().filter((note) => note.content.includes(search));
+
+    if(search !== ""){
+        cleanNotes()
+        searchResults.forEach((note) => {
+            const noteElement = createNote(note.id,note.content);
+            notesContainer.appendChild(noteElement);
+        })
+        return;
+    }
+
+    cleanNotes();
+    showNotes();
+}
+
+function exportData(){
+    const notes = getNotes();
+    //Padrão CSV: separa o dado por , quebra a linha \n
+    const csvString = [
+        ["ID", "Conteúdo", "Fixado?"],
+        ...notes.map((note) => [note.id, note.content, note.fixed])
+    ].map((e) => e.join(",")).join("\n");
+
+    const element = document.createElement("a");
+    element.href = "data:text/csv;charset=utf-8,"+encodeURI(csvString);
+    element.target = "_blank";
+    element.download = "notes.csv";
+    element.click();
+}
+
 //Eventos
 addNoteBtn.addEventListener("click", () => addNote());
+
+searchInput.addEventListener("keyup", (e) => {
+    const search = e.target.value;
+
+    searchNotes(search);
+});
+
+noteInput.addEventListener("keydown", (e) => {
+    if(e.key === "Enter"){
+        addNote();
+    }
+});
+
+exportBtn.addEventListener("click", () => {
+    exportData();
+});
 
 //Inicialização
 showNotes();
